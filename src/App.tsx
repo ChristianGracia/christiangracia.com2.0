167c8de00cafa8d23356118056ca2f58fba992aa
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-15 00:23:40 -0400

    repo init, rewriting christiangracia.com in typescript and giving an upgrade

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import { MainInfo } from "./components/main-info/main-info.component";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <MainInfo />
+    </div>
+  );
+};
+
+export default App;
