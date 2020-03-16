32971fd2c6cd12ee79301065eb7014ad5fe448ed
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-15 21:38:49 -0400

    yarn update for heroku

--- src/components/site-info/site-info.component.jsx
+++ src/components/site-info/site-info.component.jsx
@@ -5,10 +5,18 @@ export function SiteInfo() {
   return (
     <div className={styles.siteInfoContainer}>
       <span className={styles.siteInfoText}>Site written in TypeScript</span>
-      <span className={styles.siteInfoText}>Front-end: React + Redux</span>
-      <span className={styles.siteInfoText}>Back-end API: NodeJs</span>
+      <span className={styles.siteInfoText}>
+        {" "}
+        <span style={{ color: "#F9FD06" }}>Front-end:</span> React + Redux
+      </span>
+      <span className={styles.siteInfoText}>
+        <span style={{ color: "#F9FD06" }}>Back-end API:</span> NodeJs
+      </span>
       <span className={styles.codeLink}>
-        <a href="https://github.com/ChristianGracia/christiangracia.com2.0">
+        <a
+          href="https://github.com/ChristianGracia/christiangracia.com2.0"
+          className={styles.codeLink}
+        >
           Link to this site's code
         </a>
       </span>
