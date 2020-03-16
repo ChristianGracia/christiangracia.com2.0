ebdab4735f01ecec310a47308643f51ab05247c7
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-15 23:27:47 -0400

    added animated background to contact page, push for deploy

--- src/pages/contact/contact.component.tsx
+++ src/pages/contact/contact.component.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { NBackground } from "../../components/particles/nbackground/nbackground.component";
 
 export function ContactPage() {
-  return <div>Contact</div>;
+  return (
+    <div style={{ backgroundColor: "black" }}>
+      <NBackground />
+    </div>
+  );
 }
