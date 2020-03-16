82f9adc847d11db8856e8dff5adab6aa8d0652cd
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-15 21:46:16 -0400

    added other pages

--- src/components/explore-button/explore-button.component.tsx
+++ src/components/explore-button/explore-button.component.tsx
@@ -8,7 +8,7 @@ export function ExploreButton() {
     <Fade bottom>
       <div className={styles.buttonContainer}>
         <Button className={styles.exploreButton} variant="light">
-          Click here to see what I do <i class="far fa-hand-pointer"></i>
+          Click here to see what I do <i className="far fa-hand-pointer"></i>
         </Button>
       </div>
     </Fade>
