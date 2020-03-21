58bddd6fc04413a65e325bd22db2f55cb98882c9
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-21 01:01:29 -0400

    rendering all github data correctly

--- src/components/github/github.component.tsx
+++ src/components/github/github.component.tsx
@@ -27,9 +27,44 @@ export function Github() {
   const repoItems =
     githubData != undefined
       ? githubData.map((repo: any) => (
-          <div style={{}}>
-            <p>hi</p>
+          <div key={repo.id} style={{}}>
+            <Fade top>
+              <div
+                key={repo.id}
+                className="card card-body mb-2 bg-light ml-auto mr-auto pr-3"
+                style={{ margin: 0, paddingRight: 0, width: "88vw" }}
+              >
+                <div className="row">
+                  <div className="col-lg-6" style={{}}>
+                    <h4 style={{ color: "white" }}>
+                      <Link
+                        to={`//${repo.html_url.substr(7)}`}
+                        className="text-info"
+                        target="_blank"
+                      >
+                        <p style={{ color: "#3993EC" }}>{repo.name}</p>
+                      </Link>
+                    </h4>
+                    <p>{repo.description}</p>
+                  </div>
 
+                  <div className="col-md-6" style={{}}>
+                    <span className="badge badge-info mr-1">
+                      <i className="fas fa-star"></i> Stars:{" "}
+                      {repo.stargazers_count}
+                    </span>
+                    <span className="badge badge-secondary mr-1">
+                      <i className="fas fa-eye"></i> Watchers:{" "}
+                      {repo.watchers_count}
+                    </span>
+                    <span className="badge badge-success">
+                      <i className="fas fa-code-branch"></i> Forks:{" "}
+                      {repo.forks_count}
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </Fade>
             <div style={{ padding: 30 }}></div>
           </div>
         ))
