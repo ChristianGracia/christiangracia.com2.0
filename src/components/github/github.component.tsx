51ccde98bb72338c6e4155ecc0ec8334b0e1429e
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-21 01:00:21 -0400

    rendering fetched github details on front-end

--- src/components/github/github.component.tsx
+++ src/components/github/github.component.tsx
@@ -3,25 +3,41 @@ import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 import { Button } from "react-bootstrap";
 // import "./github.styles.scss";
-interface repoInfo {}
+interface IRepoInfo {
+  repoName: string;
+}
 
 export function Github() {
   const fetchDataAction = async () => {
     const clientId = require("../../config/keys").clientId;
     const clientSecret = require("../../config/keys").clientSecret;
-    const URL = `https://api.github.com/users/ChristianGracia/repos?per_page=10&sort=createdasc&client_id=${clientId}&client_secret=${clientSecret}`;
+    const URL = `https://api.github.com/users/ChristianGracia/repos?per_page=16&sort=createdasc&client_id=${clientId}&client_secret=${clientSecret}`;
 
     const data = await fetch(URL);
     const dataJSON = await data.json();
     setGithubData(dataJSON);
     console.log(dataJSON);
   };
-  const [githubData, setGithubData] = useState<object>({});
+  const [githubData, setGithubData] = useState<Array<object>>();
   useState(() => {
-    // setGithubData({ one: "1" });
     console.log(githubData);
     fetchDataAction();
   });
 
-  return <div style={{ fontSize: 30, color: "white" }}>Hi</div>;
+  const repoItems =
+    githubData != undefined
+      ? githubData.map((repo: any) => (
+          <div style={{}}>
+            <p>hi</p>
+
+            <div style={{ padding: 30 }}></div>
+          </div>
+        ))
+      : null;
+
+  return (
+    <div>
+      <div style={{ fontSize: 30, color: "white" }}>{repoItems}</div>
+    </div>
+  );
 }
