b5d2a5f0b57f314b3afa6120d22ecc867cdd14ec
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-20 23:32:59 -0400

    getting my current github repo data into the state of a functional component

--- src/components/github/github.component.tsx
+++ src/components/github/github.component.tsx
@@ -3,10 +3,14 @@ import { Link } from "react-router-dom";
 import Fade from "react-reveal/Fade";
 import { Button } from "react-bootstrap";
 // import "./github.styles.scss";
+interface repoInfo {}
 
 export function Github() {
   const fetchDataAction = async () => {
-    const URL = "";
+    const clientId = require("../../config/keys").clientId;
+    const clientSecret = require("../../config/keys").clientSecret;
+    const URL = `https://api.github.com/users/ChristianGracia/repos?per_page=10&sort=createdasc&client_id=${clientId}&client_secret=${clientSecret}`;
+
     const data = await fetch(URL);
     const dataJSON = await data.json();
     setGithubData(dataJSON);
