09505c295fadee9a536a61eb930fb12c768e8513
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-20 23:23:54 -0400

    making api calls with hooks in github component

--- src/pages/projects/projects.component.tsx
+++ src/pages/projects/projects.component.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { SBackground } from "../../components/particles/sbackground/sbackground.component";
 import { BuiltSites } from "../../components/built-sites/built-sites.component";
+import { Github } from "../../components/github/github.component";
 
 export function ProjectsPage() {
   return (
     <div style={{ backgroundColor: "black" }}>
       <BuiltSites />
       <div>
+        <Github />
         <SBackground />
       </div>
     </div>
