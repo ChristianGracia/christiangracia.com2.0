68dfd6f58dde93302cb786a50b7069ae39c551e9
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-21 03:23:37 -0400

    added css for main page to look good on all devices

--- src/pages/main/main.component.tsx
+++ src/pages/main/main.component.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { ProjectsPage } from "../projects/projects.component";
 import { AboutPage } from "../about/about.component";
+import styles from "./main.module.scss";
 
 export function MainPage() {
   return (
-    <div style={{ backgroundColor: "black" }}>
+    <div className={styles.pageContainer} style={{ backgroundColor: "black" }}>
       <div>
         <ProjectsPage />
       </div>
