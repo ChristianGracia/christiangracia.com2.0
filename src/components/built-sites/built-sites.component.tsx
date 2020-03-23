cd5470a2f2da978ed77d03c81f253d387d16f137
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-22 20:17:19 -0400

    added carousel

--- src/components/built-sites/built-sites.component.tsx
+++ src/components/built-sites/built-sites.component.tsx
@@ -1,73 +1,40 @@
 import React from "react";
 import * as Fade from "react-reveal/Fade";
 import styles from "./built-sites.module.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 // import { ProjectDisplay } from "../common/project-display/project-display.component";
-import Slider from "react-animated-slider";
+
+// import Slider from "react-animated-slider";
 
 const nflPic = require("../../assets/nflpic.PNG");
 const onabeatPic = require("../../assets/onabeatpic.PNG");
+// const content = [
+//   {
+//     image: nflPic
+//   },
+//   {
+//     image: onabeatPic
+//   }
+// ];
 
 export function BuiltSites(): JSX.Element {
-  const content = [
-    {
-      title: "Committed to Excellence",
-      description:
-        "With over 10 years of experience, we work with our clients to make their ideas come true",
-
-      image: nflPic
-    }
-    // {
-    //   title: "Jobs Big or Small, We Got You Covered",
-    //   description: "",
-
-    //   image: onabeatPic
-    // }
-  ];
   return (
     <Fade left cascade>
       <div className={styles.sitesContainer}>
         <span className={styles.sitesHeader}>Other Sites</span>
-        <span
-          className={styles.sitesDiv}
-          style={{ marginTop: 30, marginBottom: 30 }}
-        >
-          <a className=" btn btn-danger" href="https://www.onabeat.com">
-            onabeat.com
-          </a>
-        </span>
 
-        <Slider className="slider-wrapper" infinite={true} autoplay={2000}>
-          {content.map((item: any, index: number) => (
-            <div
-              key={index}
-              className="slider-content"
-              style={{
-                background: `url('${item.image}') no-repeat center center`
-              }}
-            >
-              <div className="inner">
-                <div style={{}}>
-                  <h1>{item.title}</h1>
-                  <p>{item.description}</p>
-                </div>
-                <div>
-                  <div style={{}}>
-                    <div className="" style={{ marginTop: "10vh" }}>
-                      hi
-                    </div>
-                    {/* <div className="finance-text">
-                      <Wave
-                        text="Available"
-                        effect="stretch"
-                        effectChange={1.2}
-                      />
-                    </div> */}
-                  </div>
-                </div>
-              </div>
+        <div style={{ position: "relative" }}>
+          <Carousel>
+            <div>
+              <img src={onabeatPic} />
             </div>
-          ))}
-        </Slider>
+            <div>
+              <img src={nflPic} />
+            </div>
+          </Carousel>
+        </div>
+        {/*
         <div>
           <img
             className={styles.siteImages}
@@ -91,6 +58,15 @@ export function BuiltSites(): JSX.Element {
             nflandscaping.com
           </a>
         </span>
+        <span
+          className={styles.sitesDiv}
+          style={{ marginTop: 30, marginBottom: 30 }}
+        >
+          <a className=" btn btn-danger" href="https://www.onabeat.com">
+            onabeat.com
+          </a>
+        </span>
+              */}
       </div>
     </Fade>
   );
