c38001bc6aaf13b606f5fd0d394a0e405ce3161e
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-24 22:07:09 -0400

    refactored this site components inline styling

--- src/components/this-site/this-site.component.tsx
+++ src/components/this-site/this-site.component.tsx
@@ -1,48 +1,29 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+
+import styles from "./this-site.module.scss";
 const cg = require("../../assets/christiangraciacode.PNG");
 
 export function ThisSite() {
   return (
-    <div style={{ position: "relative", padding: 20 }}>
-      <div style={{ textAlign: "center" }}>
-        <span
-          style={{
-            fontWeight: "bold",
-            fontSize: 30,
-            marginBottom: 30,
-            borderBottom: "solid 1px white",
-            maxWidth: 700,
-            minWidth: 280,
-            margin: "0 auto",
-            color: "#feff01"
-          }}
-        >
-          christiangracia.com
-        </span>
+    <div className={styles.pageContainer}>
+      <div>
+        <span className={styles.siteHeader}>christiangracia.com</span>
       </div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          marginTop: 40,
-
-          textAlign: "center"
-        }}
-      >
-        <div style={{ margin: "0 auto" }}>
+      <div className={styles.container}>
+        <div>
           <img
             src={cg}
-            style={{ width: "80vw", maxWidth: 600 }}
+            className={styles.siteImage}
             alt="A view of this website's code written in TypeScript"
           />
         </div>
-        <span style={{ marginTop: 30, color: "white" }}>
+        <span className={styles.siteText}>
           This site is written in Typescript using only functional components
           and React Hooks. React/Redux + Sass front-end with a NodeJS back-end
           API.
         </span>
-        <div style={{ marginTop: 30 }}>
+        <div className={styles.buttonDiv}>
           <Button
             variant="danger"
             onClick={() =>
