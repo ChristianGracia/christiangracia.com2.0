ccc13c0e08e8bb2a8ea435ce2228b0b3f11778cb
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-25 19:37:34 -0400

    contact form clean

--- src/components/contact-form/contact-form.component.tsx
+++ src/components/contact-form/contact-form.component.tsx
@@ -6,12 +6,7 @@ import styles from "./contact-form.module.scss";
 
 import React from "react";
 
-// interface IMessage {
-//   text: string;
-//   email: string;
-// }
 export function ContactForm(): JSX.Element {
-  // const [message, setMessage] = React.useState<string>("");
   return (
     <div style={{ position: "relative" }}>
       <div className={styles.contactPageContainer}>
@@ -30,13 +25,7 @@ export function ContactForm(): JSX.Element {
           <Jump>
             <form action="https://sendpoint.io/id/gWFzbxh9O" method="POST">
               <div className={styles.formRow}>
-                <input
-                  type="email"
-                  name="email"
-                  //   value={this.state.email}
-                  //   onChange={this.onChange}
-                  placeholder="Email"
-                />
+                <input type="email" name="email" placeholder="Email" />
               </div>
 
               <div
@@ -52,21 +41,10 @@ export function ContactForm(): JSX.Element {
                   cols={30}
                   name="body"
                   placeholder="Leave a message"
-                  //   value={this.state.body}
-                  //   onChange={this.onChange}
                 />
               </div>
               <div className={styles.formRow}>
-                <Button
-                  variant="danger"
-                  type="submit"
-                  //   onClick={e => {
-                  //     if (body === "" || email === "") {
-                  //       e.preventDefault();
-                  //       window.alert("Please fill out form");
-                  //     }
-                  //   }}
-                >
+                <Button variant="danger" type="submit">
                   Send
                 </Button>
               </div>
@@ -77,87 +55,3 @@ export function ContactForm(): JSX.Element {
     </div>
   );
 }
-
-// export default class Contact extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       body: "",
-//       email: ""
-//     };
-//     this.onChange = this.onChange.bind(this);
-//   }
-
-//   onChange(e) {
-//     this.setState({ [e.target.name]: e.target.value });
-//   }
-
-//   render() {
-//     const { body, email } = this.state;
-//     return (
-//       <div className="contact-page-container">
-//         <div className="row" style={{}}>
-//           <i
-//             className="fas fa-mail-bulk"
-//             style={{ fontSize: 80, color: "white", marginTop: 30 }}
-//           ></i>
-//         </div>
-//         <LightSpeed left>
-//           <div style={{ marginTop: 20 }}>
-//             <span className="form-label">Leave me a message!</span>
-//           </div>
-//         </LightSpeed>
-
-//         <div className="contact-form-container">
-//           <Jump>
-//             <form action="https://sendpoint.io/id/gWFzbxh9O" method="POST">
-//               <div className="row form-row-aligner">
-//                 {this.state.email === "" ? null : (
-//                   <span className="form-label">Email: </span>
-//                 )}
-//                 <input
-//                   type="email"
-//                   name="email"
-//                   value={this.state.email}
-//                   onChange={this.onChange}
-//                   placeholder="Email"
-//                 />
-//               </div>
-//               {this.state.body === "" ? null : (
-//                 <div className="row form-row">
-//                   <span className="form-label">Message</span>
-//                 </div>
-//               )}
-//               <div className="row form-row">
-//                 <textarea
-//                   rows="4"
-//                   cols="30"
-//                   type="text"
-//                   name="body"
-//                   placeholder="Leave a message"
-//                   value={this.state.body}
-//                   onChange={this.onChange}
-//                 />
-//               </div>
-//               <div className="row form-row">
-//                 <Button
-//                   variant="danger"
-//                   type="submit"
-//                   onClick={e => {
-//                     if (body === "" || email === "") {
-//                       e.preventDefault();
-//                       window.alert("Please fill out form");
-//                     }
-//                   }}
-//                 >
-//                   Send
-//                 </Button>
-//               </div>
-//             </form>
-//           </Jump>
-//           <div style={{ paddingBottom: 600 }}></div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
