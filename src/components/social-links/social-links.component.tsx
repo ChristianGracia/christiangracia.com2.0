165634a2f09d2b2afff7d048a129eb4052d3f079
Christian Gracia <christiangracia.student@careerdevs.com>
2020-03-30 19:32:05 -0400

    added lazy load to some font awesome icons for my socials

--- src/components/social-links/social-links.component.tsx
+++ src/components/social-links/social-links.component.tsx
@@ -7,20 +7,24 @@ export function SocialLinks(): JSX.Element {
       <a href="https://github.com/ChristianGracia">
         <span style={{ paddingRight: 5, color: "white" }}>GitHub</span>
         <span>
-          <i
-            className="fab fa-github"
-            style={{ color: "#FEFF01", fontSize: 20 }}
-          ></i>
+          <React.Suspense fallback={<div></div>}>
+            <i
+              className="fab fa-github"
+              style={{ color: "#FEFF01", fontSize: 20 }}
+            ></i>
+          </React.Suspense>
         </span>
       </a>
       <span style={{ padding: 10 }}></span>
       <a href="https://www.linkedin.com/in/christiangracia">
         <span style={{ paddingRight: 5, color: "white" }}>LinkedIn</span>
         <span>
-          <i
-            className="fab fa-linkedin"
-            style={{ color: "#FEFF01", fontSize: 20 }}
-          ></i>
+          <React.Suspense fallback={<div></div>}>
+            <i
+              className="fab fa-linkedin"
+              style={{ color: "#FEFF01", fontSize: 20 }}
+            ></i>
+          </React.Suspense>
         </span>
       </a>
       <span style={{ padding: 10 }}></span>
@@ -28,10 +32,12 @@ export function SocialLinks(): JSX.Element {
       <a href="http://www.twitter.com/CG_CODING">
         <span style={{ paddingRight: 5, color: "white" }}>Twitter</span>
         <span>
-          <i
-            className="fab fa-twitter"
-            style={{ color: "#FEFF01", fontSize: 20 }}
-          ></i>
+          <React.Suspense fallback={<div></div>}>
+            <i
+              className="fab fa-twitter"
+              style={{ color: "#FEFF01", fontSize: 20 }}
+            ></i>
+          </React.Suspense>
         </span>
       </a>
     </div>
