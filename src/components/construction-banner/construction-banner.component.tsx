295a71a49476e8545052d2954fccea2cb8078748
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-06 21:31:48 -0400

    last commit on this website ever, all further work on my website will be on my new repo: christiangracia3.0

--- src/components/construction-banner/construction-banner.component.tsx
+++ src/components/construction-banner/construction-banner.component.tsx
@@ -3,18 +3,23 @@ import styles from "./construction-banner.module.scss";
 
 export function ConstructionBanner(): JSX.Element {
   return (
-    <div>
-      <span className={styles.siteInfoText}>
-        <span className={styles.techs}>
-          Starting writing new website 7/6/2020. Coming soon
-        </span>{" "}
-        <a
-          href="https://github.com/ChristianGracia/christiangracia3.0"
-          style={{ color: "white" }}
-        >
-          Link to new site code
-        </a>
-      </span>
+    <div className={styles.constructionContainer}>
+      <div style={{ padding: 20 }}></div>
+      <div className={"alert alert-warning"} role="alert">
+        <span className={styles.siteInfoText}>
+          <span className={styles.techs}>7/6/2020 | Writing new website |</span>{" "}
+        </span>
+
+        <span className={styles.siteInfoText}>
+          Coming soon →{" "}
+          <a
+            href="http://christiangracia-staging.herokuapp.com/"
+            style={{ color: "red", fontWeight: "bold" }}
+          >
+            Preview{" "}
+          </a>
+        </span>
+      </div>
     </div>
   );
 }
